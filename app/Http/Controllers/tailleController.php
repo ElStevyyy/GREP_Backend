2a59b7ec112c6aeacf151ed32a99cedb3b12abba
zabraf <lopesraphi@gmail.com>
2022-04-07 07:36:36 +0000

    added Orderby for tailles

--- app/Http/Controllers/tailleController.php
+++ app/Http/Controllers/tailleController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Taille;
+
 class tailleController extends Controller
 {
     public function index()
     {
-        return Taille::all();
+        return \DB::table('tailles')->orderByraw('LENGTH(taille) asc')->get();
     }
 }
